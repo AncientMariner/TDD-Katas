63f39ffd300827035d9d03807f90b2eaba41d9f3
AncientMariner <xander.lysak@gmail.com>
2014-05-11 17:08:24 +0300

    simple number recognition added

--- src/main/java/org/xander/number/RomanNumeral.java
+++ src/main/java/org/xander/number/RomanNumeral.java
@@ -4,4 +4,69 @@ public class RomanNumeral {
     public static void main(String[] args) {
         System.out.println("Arabic numerals into Roman Numbers");
     }
+
+    public String convert() {
+        return "nothing was sent to convert";
+    }
+
+    public String convertOneDigit(String oneDigitNumber) {
+        String romanNumber = null;
+        switch (oneDigitNumber) {
+            case "0": romanNumber = ""; break;
+            case "1": romanNumber = "I"; break;
+            case "2": romanNumber = "II"; break;
+            case "3": romanNumber = "III"; break;
+            case "4": romanNumber = "IV"; break;
+            case "5": romanNumber = "V"; break;
+            case "6": romanNumber = "VI"; break;
+            case "7": romanNumber = "VII"; break;
+            case "8": romanNumber = "VIII"; break;
+            case "9": romanNumber = "IX"; break;
+            default: romanNumber = "there is no such a number";
+        }
+        return romanNumber;
+    }
+
+    public String convertTwoDigit(String twoDigitNumber) {
+        String romanNumber = null;
+        switch (twoDigitNumber) {
+            case "10": romanNumber = "X"; break;
+            case "20": romanNumber = "XX"; break;
+            case "30": romanNumber = "XXX"; break;
+            case "40": romanNumber = "XL"; break;
+            case "50": romanNumber = "L"; break;
+            case "60": romanNumber = "LX"; break;
+            case "70": romanNumber = "LXX"; break;
+            case "80": romanNumber = "LXXX"; break;
+            case "90": romanNumber = "XC"; break;
+            default: romanNumber = "there is no such a number";
+        }
+        return romanNumber;
+    }
+
+    public String convertThreeDigit(String threeDigitNumber) {
+        String romanNumber = null;
+        switch (threeDigitNumber) {
+            case "100": romanNumber = "C"; break;
+            case "200": romanNumber = "CC"; break;
+            case "300": romanNumber = "CCC"; break;
+            case "400": romanNumber = "CD"; break;
+            case "500": romanNumber = "D"; break;
+            case "600": romanNumber = "DC"; break;
+            case "700": romanNumber = "DCC"; break;
+            case "800": romanNumber = "DCCC"; break;
+            case "900": romanNumber = "CM"; break;
+            default: romanNumber = "there is no such a number";
+        }
+        return romanNumber;
+    }
+
+    public String convertFourDigit(String fourDigitNumber) {
+        String romanNumber = null;
+        switch (fourDigitNumber) {
+            case "1000": romanNumber = "M"; break;
+            default: romanNumber = "there is no such a number";
+        }
+        return romanNumber;
+    }
 }
--- src/test/java/org/xander/number/RomanNumeralTest.java
+++ src/test/java/org/xander/number/RomanNumeralTest.java
@@ -1,13 +1,158 @@
 package org.xander.number;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class RomanNumeralTest {
+    RomanNumeral romanNumeral;
+
+    @Before
+    public void setUp() {
+        romanNumeral = new RomanNumeral();
+    }
+
     @Test
     public void testRomanNumeral() {
-        RomanNumeral romanNumeral = new RomanNumeral();
         assertNotNull(romanNumeral);
+    }
+
+    @Test
+    public void testNothing() {
+        String nothing = romanNumeral.convert();
+        assertEquals("nothing was sent to convert", nothing);
+    }
+
+    @Test
+    public void testOneDigit() {
+        String one = "1";
+        String oneRoman = romanNumeral.convertOneDigit(one);
+        assertEquals("I", oneRoman);
+
+        String two = "2";
+        String twoRoman = romanNumeral.convertOneDigit(two);
+        assertEquals("II", twoRoman);
+
+        String three = "3";
+        String threeRoman = romanNumeral.convertOneDigit(three);
+        assertEquals("III", threeRoman);
+
+        String four = "4";
+        String fourRoman = romanNumeral.convertOneDigit(four);
+        assertEquals("IV", fourRoman);
+
+        String five = "5";
+        String fiveRoman = romanNumeral.convertOneDigit(five);
+        assertEquals("V", fiveRoman);
+
+        String six = "6";
+        String sixRoman = romanNumeral.convertOneDigit(six);
+        assertEquals("VI", sixRoman);
+
+        String seven = "7";
+        String sevenRoman = romanNumeral.convertOneDigit(seven);
+        assertEquals("VII", sevenRoman);
+
+        String eight = "8";
+        String eightRoman = romanNumeral.convertOneDigit(eight);
+        assertEquals("VIII", eightRoman);
+
+        String nine = "9";
+        String nineRoman = romanNumeral.convertOneDigit(nine);
+        assertEquals("IX", nineRoman);
+    }
+
+    @Test
+    public void testTwoDigit() {
+        String ten = "10";
+        String tenRoman = romanNumeral.convertTwoDigit(ten);
+        assertEquals("X", tenRoman);
+
+        String twenty = "20";
+        String twentyRoman = romanNumeral.convertTwoDigit(twenty);
+        assertEquals("XX", twentyRoman);
+
+        String thirty = "30";
+        String thirtyRoman = romanNumeral.convertTwoDigit(thirty);
+        assertEquals("XXX", thirtyRoman);
+
+        String forty = "40";
+        String fortyRoman = romanNumeral.convertTwoDigit(forty);
+        assertEquals("XL", fortyRoman);
+
+        String fifty = "50";
+        String fiftyRoman = romanNumeral.convertTwoDigit(fifty);
+        assertEquals("L", fiftyRoman);
+
+        String sixty = "60";
+        String sixtyRoman = romanNumeral.convertTwoDigit(sixty);
+        assertEquals("LX", sixtyRoman);
+
+        String seventy = "70";
+        String seventyRoman = romanNumeral.convertTwoDigit(seventy);
+        assertEquals("LXX", seventyRoman);
+
+        String eighty = "80";
+        String eightyRoman = romanNumeral.convertTwoDigit(eighty);
+        assertEquals("LXXX", eightyRoman);
+
+        String ninety = "90";
+        String ninetyRoman = romanNumeral.convertTwoDigit(ninety);
+        assertEquals("XC", ninetyRoman);
+    }
+
+    @Test
+    public void testThreeDigit() {
+        String hundred = "100";
+        String hundredRoman = romanNumeral.convertThreeDigit(hundred);
+            assertEquals("C", hundredRoman);
+
+        String twoHundred = "200";
+        String twoHundredRoman = romanNumeral.convertThreeDigit(twoHundred);
+        assertEquals("CC", twoHundredRoman);
+
+        String threeHundred = "300";
+        String threeHundredRoman = romanNumeral.convertThreeDigit(threeHundred);
+        assertEquals("CCC", threeHundredRoman);
+
+        String fourHundred = "400";
+        String fourHundredRoman = romanNumeral.convertThreeDigit(fourHundred);
+        assertEquals("CD", fourHundredRoman);
+
+        String fiveHundred = "500";
+        String fiveHundredRoman = romanNumeral.convertThreeDigit(fiveHundred);
+        assertEquals("D", fiveHundredRoman);
+
+        String sixHundred = "600";
+        String sixHundredRoman = romanNumeral.convertThreeDigit(sixHundred);
+        assertEquals("DC", sixHundredRoman);
+
+        String sevenHundred = "700";
+        String sevenHundredRoman = romanNumeral.convertThreeDigit(sevenHundred);
+        assertEquals("DCC", sevenHundredRoman);
+
+        String eightHundred = "800";
+        String eightHundredRoman = romanNumeral.convertThreeDigit(eightHundred);
+        assertEquals("DCCC", eightHundredRoman);
+
+        String nineHundred = "900";
+        String nineHundredRoman = romanNumeral.convertThreeDigit(nineHundred);
+        assertEquals("CM", nineHundredRoman);
+    }
+
+    @Test
+    public void testFourDigit() {
+        String hundred = "1000";
+        String hundredRoman = romanNumeral.convertFourDigit(hundred);
+        assertEquals("M", hundredRoman);
+    }
+
+    @Test
+    public void testNegative() {
+        String notANumber = "a";
+        String numberRoman = romanNumeral.convertFourDigit(notANumber);
+        assertEquals("there is no such a number", numberRoman);
     }
 }
