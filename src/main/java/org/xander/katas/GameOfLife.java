6898d5540dfaaafaabf93dff215d337f2d7cc1fd
AncientMariner <xander.lysak@gmail.com>
2014-05-24 12:57:17 +0300

    game of life starting grid, first generation

--- src/main/java/org/xander/katas/GameOfLife.java
+++ src/main/java/org/xander/katas/GameOfLife.java
@@ -1,4 +1,62 @@
 package org.xander.katas;
 
 public class GameOfLife {
+    public String drawCell(int dimensionY, int dimensionX) {
+        String result = "";
+        for (int i = 0; i < dimensionY; i++) {
+            for (int j = 0; j < dimensionX; j++) {
+                result += ".";
+            }
+            result += "\n";
+        }
+        return result;
+    }
+
+    public String buildFirstGeneration(int liveCellPositionY, int liveCellPositionX, String cell) {
+        char[] livingCell = cell.toCharArray();
+        int dimensionY = calculateYSize(cell);
+        int dimensionX = calculateXSize(cell);
+
+
+        for (int i = 0; i <= dimensionY; i++) {
+            for (int j = 0; j <= dimensionX; j++) {
+                if(i == (liveCellPositionY - 1) && j == (liveCellPositionX - 1)){
+                    //dimensionX + 1 because of \n char
+                    livingCell[i * (dimensionX + 1) + j] = '*';
+                }
+            }
+        }
+        String cellWithLivingElement = String.valueOf(livingCell);
+
+        return cellWithLivingElement;
+    }
+
+    public String buildTheWholeGeneration(int[][] arrayOfCoordinates, String cell) {
+        String result = cell;
+        for (int[] yCoordinates : arrayOfCoordinates) {
+            result = buildFirstGeneration(yCoordinates[0], yCoordinates[1], result);
+        }
+
+        return result;
+    }
+
+    private int calculateXSize(String cell) {
+        int size = 0;
+        for (char element : cell.toCharArray()) {
+            if (element != '\n') {
+                size++;
+            } else break;
+        }
+        return size;
+    }
+
+    private int calculateYSize(String cell) {
+        int size = 0;
+        for (char element : cell.toCharArray()) {
+            if (element == '\n') {
+                size++;
+            }
+        }
+        return size;
+    }
 }
--- src/test/java/org/xander/katas/GameOfLifeTest.java
+++ src/test/java/org/xander/katas/GameOfLifeTest.java
@@ -3,6 +3,7 @@ package org.xander.katas;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
 public class GameOfLifeTest {
@@ -17,5 +18,49 @@ public class GameOfLifeTest {
     public void testBasic() {
         assertNotNull(gameOfLife);
     }
+
+    @Test
+    public void testOutPutEmptyCell() {
+        String expectedCell = "........\n" + "........\n" + "........\n" + "........\n";
+        int dimensionX = 8;
+        int dimensionY = 4;
+        String actualCell = gameOfLife.drawCell(dimensionY,dimensionX);
+
+        assertEquals(expectedCell, actualCell);
+    }
+
+    @Test
+    public void testFirstPositions() {
+        String expectedCell = ".*......\n" + "........\n" + "........\n" + "........\n";
+        String cell = gameOfLife.drawCell(4,8);
+        int liveCellPositionX = 2;
+        int liveCellPositionY = 1;
+        String actualCell = gameOfLife.buildFirstGeneration(liveCellPositionY, liveCellPositionX, cell);
+
+        assertEquals(expectedCell, actualCell);
+
+        int liveCellPositionX2= 2;
+        int liveCellPositionY2 = 2;
+        String expectedCell2 = ".*......\n" + ".*......\n" + "........\n" + "........\n";
+        String actualCell2 = gameOfLife.buildFirstGeneration(liveCellPositionY2, liveCellPositionX2, actualCell);
+
+        assertEquals(expectedCell2, actualCell2);
+
+        int liveCellPositionX3= 3;
+        int liveCellPositionY3 = 2;
+        String expectedCell3 = ".*......\n" + ".**.....\n" + "........\n" + "........\n";
+        String actualCell3 = gameOfLife.buildFirstGeneration(liveCellPositionY3, liveCellPositionX3, actualCell2);
+
+        assertEquals(expectedCell3, actualCell3);
+    }
+
+    @Test
+    public void testWholeGenerationAtOnce() {
+        String expectedCell = ".*......\n" + ".**.....\n" + "........\n" + "........\n";
+        int[][] input = {{1,2}, {2,2}, {2,3}};
+        String result = gameOfLife.buildTheWholeGeneration(input, gameOfLife.drawCell(4, 8));
+        assertEquals(expectedCell,result);
+    }
+
 
 }
