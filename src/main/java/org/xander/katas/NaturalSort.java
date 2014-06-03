ebaefab045d99cb2d2abc3ffadd41c6acb6bbaff
AncientMariner <xander.lysak@gmail.com>
2014-06-03 08:42:20 +0300

    natural sort refactor, moving sorting class inside

--- src/main/java/org/xander/katas/NaturalSort.java
+++ src/main/java/org/xander/katas/NaturalSort.java
@@ -5,17 +5,21 @@ import java.util.List;
 
 public class NaturalSort {
     private static NaturalSort naturalSort = new NaturalSort();
-    private MyNaturalSort myNaturalSort;
-
-    private NaturalSort() {
-        myNaturalSort = new MyNaturalSort();
-    }
 
     public static NaturalSort getInstance() {
         return naturalSort;
     }
 
     public List<String> sortNaturally(List<String> listToSort) {
+        class MyNaturalSort implements Comparator<String> {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        }
+
+        MyNaturalSort myNaturalSort = new MyNaturalSort() ;
+
         for (int numberOfPasses = 1; numberOfPasses < listToSort.size(); numberOfPasses++) {
             for (int i = 0; i < listToSort.size() - numberOfPasses; i++) {
                 if (myNaturalSort.compare(listToSort.get(i), listToSort.get(i + 1)) > 0) {
@@ -26,12 +30,5 @@ public class NaturalSort {
             }
         }
         return listToSort;
-    }
-}
-
-class MyNaturalSort implements Comparator<String> {
-    @Override
-    public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
     }
 }
