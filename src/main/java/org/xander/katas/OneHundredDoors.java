af7c69e689b5d9c0ea3ab4491f2bbe09fda21fc0
AncientMariner <xander.lysak@gmail.com>
2014-06-03 20:23:38 +0300

    100 doors

---
+++ src/main/java/org/xander/katas/OneHundredDoors.java
@@ -0,0 +1,28 @@
+package org.xander.katas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneHundredDoors {
+    List<Boolean> listOfDoors = new ArrayList<>(100);
+
+    {
+        for (int i = 0; i < 100; i++) {
+            listOfDoors.add(false);
+        }
+    }
+
+    public List<Boolean> checkAllDoors() {
+        return listOfDoors;
+    }
+
+    public void go(int j) {
+        for (int i = 0 ; i < listOfDoors.size() ; i = i + j ) {
+            listOfDoors.set(i, !listOfDoors.get(i));
+        }
+    }
+
+    public List<Boolean> getListOfDoors() {
+        return listOfDoors;
+    }
+}
---
+++ src/test/java/org/xander/katas/OneHundredDoorsTest.java
@@ -0,0 +1,95 @@
+package org.xander.katas;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OneHundredDoorsTest {
+    OneHundredDoors oneHundredDoors = new OneHundredDoors();
+
+    @Test
+    public void testBasic() {
+        assertNotNull(oneHundredDoors);
+    }
+
+    @Test
+    public void testAllAreClosedInitially() {
+        for (Boolean door : oneHundredDoors.checkAllDoors()) {
+            assertFalse(door);
+        }
+    }
+
+    @Test
+    public void testFirstGo() {
+        oneHundredDoors.go(1);
+
+        for (Boolean door : oneHundredDoors.checkAllDoors()) {
+            assertTrue(door);
+        }
+    }
+
+    @Test
+    public void testSecondGo() {
+        int second = 2;
+        oneHundredDoors.go(second);
+        List<Boolean> currentList = oneHundredDoors.getListOfDoors();
+
+        for (int i = 0; i < currentList.size() ; i = i + second) {
+            assertTrue(currentList.get(i));
+        }
+    }
+
+    @Test
+    public void testThirdGo() {
+        int third = 3;
+        oneHundredDoors.go(third);
+        List<Boolean> currentList = oneHundredDoors.getListOfDoors();
+
+        for (int i = 0; i < currentList.size() ; i = i + third) {
+            assertTrue(currentList.get(i));
+        }
+    }
+
+    @Test
+    public void testAllGoes() {
+        for (int goNumber = 1; goNumber < 100; goNumber++) {
+            oneHundredDoors.go(goNumber);
+        }
+        outputDoorsStates();
+    }
+
+    private void outputDoorsStates() {
+        List<Boolean> positiveNumbers = new ArrayList<>();
+        List<Boolean> negativeNumbers = new ArrayList<>();
+
+        for (Boolean element : oneHundredDoors.getListOfDoors()) {
+            if (element.booleanValue()) {
+                positiveNumbers.add(element);
+            } else {
+                negativeNumbers.add(element);
+            }
+        }
+
+        System.out.println("There are " + positiveNumbers.size() + " positive numbers ");
+        System.out.print("and " + negativeNumbers.size() + " negative numbers ");
+        System.out.println("\nHere are they respectively: ");
+
+
+        System.out.println("\nPositive : ");
+        List<Boolean> listOfDoors = oneHundredDoors.getListOfDoors();
+        for (int i = 0 ; i < listOfDoors.size() ; i++) {
+            if (listOfDoors.get(i))
+                System.out.print(i + ", ");
+
+        }
+        System.out.println("\nNegative : ");
+
+        for (int i = 0 ; i < listOfDoors.size() ; i++) {
+            if (!listOfDoors.get(i))
+                System.out.print(i + ", ");
+        }
+    }
+}
