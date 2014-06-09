f4ff391993b43ad2bc248a6cbf09c63d9df72d51
AncientMariner <xander.lysak@gmail.com>
2014-06-09 11:03:20 +0300

    zeckendorf number representation + new kata to do

---
+++ src/main/java/org/xander/katas/ZeckendorfNumber.java
@@ -0,0 +1,47 @@
+package org.xander.katas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZeckendorfNumber {
+    int[] fibonacciRow = new int[40];
+
+    public int[] generateFibonacci(final int threshold) {
+        final int initialNumber = 1;
+
+        fibonacciRow[0] = initialNumber;
+        fibonacciRow[1] = initialNumber;
+
+        for (int i = 2; i < threshold; i++) {
+            fibonacciRow[i] = fibonacciRow[i - 2] + fibonacciRow[i - 1];
+        }
+        return fibonacciRow;
+    }
+
+    public List<Integer> representNumber(int inputNumber) {
+        List<Integer> resultArray = new ArrayList<>();
+
+        for (int i = fibonacciRow.length - 1; i > 0; i--) {
+            if (inputNumber >= fibonacciRow[i] && inputNumber % fibonacciRow[i] >= 0) {
+                if (inputNumber == fibonacciRow[i]) {
+                    resultArray.add(fibonacciRow[i]);
+                    break;
+                }
+                if (resultArray.isEmpty()) {
+                    resultArray.add(fibonacciRow[i]);
+                } else if(!resultArray.isEmpty() && fibonacciRow[i] + sumOfAlreadyPresentElementsIn(resultArray) <= inputNumber) {
+                    resultArray.add(fibonacciRow[i]);
+                }
+            }
+        }
+        return resultArray;
+    }
+
+    private Integer sumOfAlreadyPresentElementsIn(List<Integer> resultArray) {
+        int sumOfAllElements = 0;
+        for (int element : resultArray) {
+            sumOfAllElements += element;
+        }
+        return sumOfAllElements;
+    }
+}
---
+++ src/test/java/org/xander/katas/ZeckendorfNumberTest.java
@@ -0,0 +1,142 @@
+package org.xander.katas;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class ZeckendorfNumberTest {
+    ZeckendorfNumber zeckendorfNumber;
+    final int threshold = 40;
+
+    @Before
+    public void setUp() {
+        zeckendorfNumber = new ZeckendorfNumber();
+    }
+
+    @Test
+    public void testBasic() {
+        assertNotNull(zeckendorfNumber);
+    }
+
+    @Test
+    public void testFibonacciMax20() {
+        int[] fibonacciArray = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+                    89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765,
+                    10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040,
+                    1346269, 2178309, 3524578, 5702887, 9227465,
+                    14930352, 24157817, 39088169, 63245986, 102334155};
+
+        int[] resultFibonacciRow = zeckendorfNumber.generateFibonacci(threshold);
+
+        assertEquals(threshold, resultFibonacciRow.length);
+
+        for (int i = 0; i < fibonacciArray.length; i++) {
+            assertEquals(fibonacciArray[i], resultFibonacciRow[i]);
+        }
+    }
+
+    @Test
+    public void testRepresentDistinctNumber3() {
+        int inputNumber = 3;
+
+        zeckendorfNumber.generateFibonacci(threshold);
+        List<Integer> resultArrayOfNumbers = zeckendorfNumber.representNumber(inputNumber);
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(3);
+
+        Collection expected = new ArrayList(expectedArray);
+        Collection actual = new ArrayList(resultArrayOfNumbers);
+
+        assertFalse(expected.retainAll(actual));
+
+    }
+
+    @Test
+    public void testRepresentDistinctNumber11() {
+        int inputNumber = 11;
+
+        zeckendorfNumber.generateFibonacci(threshold);
+        List<Integer> resultArrayOfNumbers = zeckendorfNumber.representNumber(inputNumber);
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(8);
+        expectedArray.add(3);
+
+        Collection expected = new ArrayList(expectedArray);
+        Collection actual = new ArrayList(resultArrayOfNumbers);
+
+        assertFalse(expected.retainAll(actual));
+    }
+
+    @Test
+    public void testRepresentDistinctNumber13() {
+        int inputNumber = 13;
+
+        zeckendorfNumber.generateFibonacci(threshold);
+        List<Integer> resultArrayOfNumbers = zeckendorfNumber.representNumber(inputNumber);
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(13);
+
+        Collection expected = new ArrayList(expectedArray);
+        Collection actual = new ArrayList(resultArrayOfNumbers);
+
+        assertFalse(expected.retainAll(actual));
+    }
+
+    @Test
+    public void testRepresentDistinctNumber25() {
+        int inputNumber = 25;
+
+        zeckendorfNumber.generateFibonacci(threshold);
+        List<Integer> resultArrayOfNumbers = zeckendorfNumber.representNumber(inputNumber);
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(21);
+        expectedArray.add(3);
+        expectedArray.add(1);
+
+        Collection expected = new ArrayList(expectedArray);
+        Collection actual = new ArrayList(resultArrayOfNumbers);
+
+        assertFalse(expected.retainAll(actual));
+    }
+
+    @Test
+    public void testRepresentDistinctNumber4563() {
+        int inputNumber = 4563;
+
+        zeckendorfNumber.generateFibonacci(threshold);
+        List<Integer> resultArrayOfNumbers = zeckendorfNumber.representNumber(inputNumber);
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(4181);
+        expectedArray.add(377);
+        expectedArray.add(5);
+
+        Collection expected = new ArrayList(expectedArray);
+        Collection actual = new ArrayList(resultArrayOfNumbers);
+
+        assertFalse(expected.retainAll(actual));
+    }
+
+    @Test
+    public void testRepresentDistinctNumber45633() {
+        int inputNumber = 45633;
+
+        zeckendorfNumber.generateFibonacci(threshold);
+        List<Integer> resultArrayOfNumbers = zeckendorfNumber.representNumber(inputNumber);
+        List<Integer> expectedArray = new ArrayList<>();
+        expectedArray.add(28657);
+        expectedArray.add(10946);
+        expectedArray.add(1597);
+        expectedArray.add(233);
+        expectedArray.add(13);
+        expectedArray.add(5);
+        expectedArray.add(1);
+
+        Collection expected = new ArrayList(expectedArray);
+        Collection actual = new ArrayList(resultArrayOfNumbers);
+
+        assertFalse(expected.retainAll(actual));
+    }
+}
