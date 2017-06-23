8c0ab8780f4291108cdcdaaf8acd66a2d6ca8cdc
AncientMariner <xander.lysak@gmail.com>
2017-06-23 12:10:29 +0300

    Update FindUniqueCharInStringTest.java

--- src/test/java/org/xander/katas/FindUniqueCharInStringTest.java
+++ src/test/java/org/xander/katas/FindUniqueCharInStringTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestTestA {
+public class FindUniqueCharInStringTest {
     FindUniqueCharInString findUniqueCharInString;
 
     @Before
