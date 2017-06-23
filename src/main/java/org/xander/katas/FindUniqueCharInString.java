f0a24c034c5bcde35e07d6b785959c991525e974
AncientMariner <xander.lysak@gmail.com>
2017-06-23 12:04:43 +0300

    find unique char in a string

---
+++ src/main/java/org/xander/katas/FindUniqueCharInString.java
@@ -0,0 +1,29 @@
+package org.xander.katas;
+
+public class TestA {
+    public char findFirstUnique(String s) {
+        int number = -1;
+        if (s.length() == 1) {
+            return s.charAt(0);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0
+                    && i + 1 < s.length()
+                    && s.charAt(i + 1) != s.charAt(i) ) {
+                number = i;
+                break;
+            }
+            if (i - 1 >= 0
+                    && s.charAt(i - 1) != s.charAt(i)
+                    && i + 1 < s.length()
+                    && s.charAt(i + 1) != s.charAt(i)) {
+                number = i;
+                break;
+            }
+        }
+        if (number < 0) {
+            throw new IllegalArgumentException("no unique element");
+        }
+        return s.charAt(number);
+    }
+}
