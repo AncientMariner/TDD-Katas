9bb91323a1b2a812c09530488b402699697a5f26
AncientMariner <xander.lysak@gmail.com>
2018-06-26 17:29:34 +0200

    Update CalculateLength.java

--- src/main/java/org/xander/katas/CalculateLength.java
+++ src/main/java/org/xander/katas/CalculateLength.java
@@ -1,9 +1,16 @@
 package org.xander.katas;
 
 public class CalculateLength {
-    private static final int bigBrickLength = 5;
-    private static final int littleBrickLength = 1;
+    //    private static final int bigBrick = 5;
+//    private static final int littleBrick = 1;
+    private int littleBrick;
+    private int bigBrick;
     private boolean result;
+
+    public CalculateLength(int bigBrick, int littleBrick) {
+        this.bigBrick = bigBrick; 
+        this.littleBrick = littleBrick; 
+    }
 
     boolean measure(int totalLength, int bigBrickNumber, int littleBrickNumber) {
         if (totalLength <= 0) {
@@ -13,36 +20,37 @@ public class CalculateLength {
             return false;
         }
 
-
         if (bigBrickNumber == 0) {
-            return oneOfNumbersIsZero(totalLength, littleBrickNumber, littleBrickLength);
+            return oneOfNumbersIsZero(totalLength, littleBrickNumber, littleBrick);
         }
 
         if (littleBrickNumber == 0) {
-            return oneOfNumbersIsZero(totalLength, bigBrickNumber, bigBrickLength);
+            return oneOfNumbersIsZero(totalLength, bigBrickNumber, bigBrick);
         }
 
-        if (totalLength > bigBrickNumber * bigBrickLength + littleBrickNumber * littleBrickLength) {
+        if (totalLength > bigBrickNumber * bigBrick + littleBrickNumber * littleBrick) {
             result = false;
-        } else if (totalLength == bigBrickNumber * bigBrickLength + littleBrickNumber * littleBrickLength) {
+        } else if (totalLength == bigBrickNumber * bigBrick + littleBrickNumber * littleBrick) {
             result = true;
         } else {
             int leftOverForSmallBricks = calculateLeftOverForSmallBricks(totalLength, bigBrickNumber);
 
-            if ((totalLength <= bigBrickNumber * bigBrickLength && totalLength >= littleBrickNumber * littleBrickLength)
-               || (totalLength >= bigBrickNumber * bigBrickLength && totalLength >= littleBrickNumber * littleBrickLength)) {
+            if ((totalLength <= bigBrickNumber * bigBrick && totalLength >= littleBrickNumber * littleBrick)
+               || (totalLength >= bigBrickNumber * bigBrick && totalLength >= littleBrickNumber * littleBrick)) {
 
                 if (!result) {
-                    if (leftOverForSmallBricks < littleBrickLength * littleBrickNumber) {
+                    if (leftOverForSmallBricks < littleBrick * littleBrickNumber) {
                         result = leftOverBricksCouldBeBuildFromExistingOnes(leftOverForSmallBricks);
-                    } else result = leftOverForSmallBricks == littleBrickLength * littleBrickNumber;
+                    } else {
+                        result = leftOverForSmallBricks == littleBrick * littleBrickNumber;
+                    }
                 }
             }
 
-            if ((totalLength <= bigBrickNumber * bigBrickLength && totalLength <= littleBrickNumber * littleBrickLength)
-               || (totalLength >= bigBrickNumber * bigBrickLength && totalLength <= littleBrickNumber * littleBrickLength)) {
+            if ((totalLength <= bigBrickNumber * bigBrick && totalLength <= littleBrickNumber * littleBrick)
+               || (totalLength >= bigBrickNumber * bigBrick && totalLength <= littleBrickNumber * littleBrick)) {
 
-                if(!result){
+                if (!result) {
                     result = leftOverBricksCouldBeBuildFromExistingOnes(leftOverForSmallBricks);
                 }
             }
@@ -52,25 +60,25 @@ public class CalculateLength {
 
     private int calculateLeftOverForSmallBricks(int totalLength, int bigBrickNumber) {
         int numberOfBricks = 0;
-        if (totalLength % bigBrickLength == 0) {
-            int requiredNumberOfBigBricks = totalLength / bigBrickLength;
+        if (totalLength % bigBrick == 0) {
+            int requiredNumberOfBigBricks = totalLength / bigBrick;
             if (requiredNumberOfBigBricks == bigBrickNumber) {
                 result = true;
             } else if (requiredNumberOfBigBricks < bigBrickNumber) {
                 numberOfBricks = requiredNumberOfBigBricks;
             }
         } else {
-            int leftOverOfBigBricks = totalLength % bigBrickLength;
-            int numberOfBigBricksNeeded = (totalLength - leftOverOfBigBricks) / bigBrickLength;
+            int leftOverOfBigBricks = totalLength % bigBrick;
+            int numberOfBigBricksNeeded = (totalLength - leftOverOfBigBricks) / bigBrick;
             if (numberOfBigBricksNeeded <= bigBrickNumber) {
                 numberOfBricks = numberOfBigBricksNeeded;
             }
         }
-        return totalLength - numberOfBricks * bigBrickLength;
+        return totalLength - numberOfBricks * bigBrick;
     }
 
     private boolean leftOverBricksCouldBeBuildFromExistingOnes(int leftOverForSmallBricks) {
-        return leftOverForSmallBricks % littleBrickLength == 0;
+        return leftOverForSmallBricks % littleBrick == 0;
     }
 
     private Boolean oneOfNumbersIsZero(int totalLength, int brickNumber, int brickLength) {
