141a7b226e69ca57fd0c6724e8ec480f75d6b75f
AncientMariner <xander.lysak@gmail.com>
2018-06-26 17:34:23 +0200

    Update RegexLearn.java

--- src/main/java/org/xander/katas/RegexLearn.java
+++ src/main/java/org/xander/katas/RegexLearn.java
@@ -6,15 +6,15 @@ public class RegexLearn {
     public boolean findTheResult(Matcher matcher) {
         boolean found = false;
         while (matcher.find()) {
-            System.out.printf("The following text is found:" +
-                                " \"%s\"; starting at " +
-                                "index %d and ending at index %d.%n%n",
+            System.out.printf("The following text is found:"
+                               + " \"%s\"; starting at "
+                               + "index %d and ending at index %d.%n%n",
                                 matcher.group(),
                                 matcher.start(),
                                 matcher.end());
             found = true;
         }
-        if(!found) {
+        if (!found) {
             System.out.println("No match found.");
             return found;
         } else {
